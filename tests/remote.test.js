9e75d995cdacb2e211681ec4030b88e27772af0f
Wil Wilsman <wil@wilsman.dev>
2024-03-14 03:23:17 -0500

    ✅ Test remote

---
+++ tests/remote.test.js
@@ -0,0 +1,10 @@
+import { fork } from 'node:child_process';
+import { use } from '../lib/harness.js';
+import { connect } from '../lib/remote.js';
+
+use(connect(() => [1, 2].map(() => (
+  fork('./tests/harness.test.js', {
+    env: { ...process.env, __MOONSHINER_REMOTE__: 'process' },
+    silent: true
+  })
+))));
