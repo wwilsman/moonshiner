6e26750298b1b51351b7e7219508c8f354f318a2
Wil Wilsman <wil@wilsman.dev>
2024-03-14 03:22:51 -0500

    ✅ Test only

---
+++ tests/only.test.js
@@ -0,0 +1,49 @@
+import { describe, test } from '../lib/harness.js';
+
+describe('should run', () => {
+  test('should not run', () => {
+    throw new Error('should have skipped');
+  });
+
+  test.only('should run first', () => {
+    // nothing to assert
+  });
+
+  test('should also not run', () => {
+    throw new Error('should have also skipped');
+  });
+
+  describe.only('should run these too', () => {
+    test('should run second', () => {});
+
+    test.only('should run third', async t => {
+      await t.test('should run fourth', { only: true }, () => {});
+
+      await t.test('should not run subtest', () => {
+        throw new Error('should have skipped subtest');
+      });
+    });
+
+    describe('should also run this', () => {
+      test('should run fifth', () => {});
+    });
+  });
+
+  describe('should not run these', () => {
+    test('should not run this', () => {
+      throw new Error('should have skipped this');
+    });
+
+    test('should not run that', () => {
+      throw new Error('should have skipped that');
+    });
+
+    describe('except for one of these', () => {
+      test.only('should run sixth', () => {});
+
+      test('should not run this one', () => {
+        throw new Error('should have skipped this one');
+      });
+    });
+  });
+});
