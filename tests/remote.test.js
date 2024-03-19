446934b005c6290bdbf8e62f595675798e223b80
Wil Wilsman <wil@wilsman.dev>
2024-03-19 02:08:51 -0500

    ♻️ Rename remote connections

--- tests/remote.test.js
+++ tests/remote.test.js
@@ -1,6 +1,6 @@
 import { fork } from 'node:child_process';
 import { use } from '../lib/harness.js';
-import { connect } from '../lib/remote.js';
+import { connect } from '../lib/connection.js';
 
 use(connect(() => [1, 2].map(() => (
   fork('./tests/harness.test.js', {
