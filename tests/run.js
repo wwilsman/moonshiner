5a3350f0b929b7961966b6a5ccc757dd920ff2ec
Wil Wilsman <wil@wilsman.dev>
2024-03-20 22:19:32 -0500

    ✅ Add combined test file to run all other tests

---
+++ tests/run.js
@@ -0,0 +1,63 @@
+import { fork } from 'node:child_process';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import { strict as assert } from 'node:assert/strict';
+import { join } from 'node:path';
+
+import { describe, test } from '../lib/harness.js';
+import { DeferredPromise } from '../lib/util/promise.js';
+
+const updateSnapshots = process.argv.includes('--update-snapshots');
+
+const snapshotDirectory = 'tests/__snapshots__';
+await mkdir(snapshotDirectory, { recursive: true });
+
+async function runTests(file, {
+  expectExitCode = 0
+} = {}) {
+  let name = file.replaceAll(/^tests\//g, '');
+  let snapshot = join(snapshotDirectory, name);
+
+  let child = fork(file, { silent: true });
+  let deferred = new DeferredPromise();
+  let output = [];
+
+  child.stdout.on('data', chunk => output.push(...(
+    chunk.toString().split(/(?<=\n)/).map(stdout => ({ stdout })))));
+  child.stderr.on('data', chunk => output.push(...(
+    chunk.toString().split(/(?<=\n)/).map(stderr => ({ stderr })))));
+  child.on('error', deferred.reject);
+  child.on('exit', deferred.resolve);
+
+  let exitCode = await deferred;
+  let result = output.map(l => JSON.stringify(l)).join('\n');
+  let expected = await readFile(snapshot, 'utf-8').catch(() => '');
+
+  if (updateSnapshots && expected !== result) writeFile(snapshot, result);
+  else assert.equal(result, expected);
+
+  assert.equal(exitCode, expectExitCode, (
+    `Expected exit code ${expectExitCode} but received ${exitCode}`
+  ));
+};
+
+describe('Moonshiner', { timeout: 0 }, () => {
+  test('test harness', async () => {
+    await runTests('tests/harness.test.js');
+  });
+
+  test('isolated tests', async () => {
+    await runTests('tests/only.test.js');
+  });
+
+  test('skipped tests', async () => {
+    await runTests('tests/skip.test.js');
+  });
+
+  test('remote tests', async () => {
+    await runTests('tests/remote.test.js');
+  });
+
+  test('browser tests', async () => {
+    await runTests('tests/browser.test.js');
+  });
+});
