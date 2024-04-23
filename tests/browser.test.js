820b5a853512593e7edb26ebba7877304a4ded70
Wil Wilsman <wil@wilsman.dev>
2024-04-22 21:53:32 -0500

    ✅ Update browser tests

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,13 +1,13 @@
 import { describe, it, after, configure } from 'moonshiner';
 
 describe('merges unique tests', () => {
-  it('reports once', () => {
-    console.log('running');
+  it('reports once', t => {
+    console.log('running', t.timeout());
   });
 });
 
 configure({
-  // debug: true,
+  timeout: 10_000,
 
   browsers: [
     'Firefox',
@@ -20,58 +20,56 @@ configure({
     }
   ],
 
-  server: {
-    serve: ['.', {
-      '/index.html': `
-        <!doctype html>
-        <html lang="en">
-        <body>
-          <style>
-            html, body { height: 100%; margin: 0; padding; 0; font: 2rem system-ui; }
-            #test { display: grid; place-content: center; height: 100%; }
-          </style>
-          <div id="test">Test</div>
-          <script type="module">
-            import { describe, it } from '/lib/harness.js';
+  serve: ['.', {
+    '/index.html': `
+      <!doctype html>
+      <html lang="en">
+      <body>
+        <style>
+          html, body { height: 100%; margin: 0; padding; 0; font: 2rem system-ui; }
+          #test { display: grid; place-content: center; height: 100%; }
+        </style>
+        <div id="test">Test</div>
+        <script type="module">
+          import { describe, it } from '/lib/harness.js';
 
-            describe('merges unique tests', () => {
-              it('reports once', () => {
-                console.log('running');
-              });
-
-              it('reports skipped');
-
-              it('takes a screenshot', async t => {
-                await t.screenshot();
-              });
-
-              it('should fail for existing screenshot', async t => {
-                await t.screenshot('takes a screenshot');
-              });
-
-              it('should fail for changed screenshot', async t => {
-                document.getElementById('test').innerHTML = Math.random();
-                await t.screenshot();
-              });
-
-              it(sessionStorage.getItem('__MOONSHINER_REMOTE__'), () => {});
+          describe('merges unique tests', () => {
+            it('reports once', t => {
+              console.log('running', t.timeout());
             });
 
-            if (!sessionStorage.getItem('reloaded')) setTimeout(() => {
-              sessionStorage.setItem('reloaded', true);
-              location.reload();
-            }, 500);
-          </script>
-        </body>
-        </html>`
-    }]
-  }
+            it('reports skipped');
+
+            it('takes a screenshot', async t => {
+              await t.screenshot();
+            });
+
+            it('should fail for existing screenshot', async t => {
+              await t.screenshot('merges unique tests | takes a screenshot');
+            });
+
+            it('should fail for changed screenshot', async t => {
+              document.getElementById('test').innerHTML = Math.random();
+              await t.screenshot();
+            });
+
+            it(sessionStorage.getItem('__MOONSHINER_REMOTE__'), () => {});
+          });
+
+          if (!sessionStorage.getItem('reloaded')) setTimeout(() => {
+            sessionStorage.setItem('reloaded', true);
+            location.reload();
+          }, 500);
+        </script>
+      </body>
+      </html>`
+  }]
 });
 
 after(async () => {
+  let screenshot = 'merges unique tests | should fail for changed screenshot.new.png';
   let { unlink } = await import('fs/promises');
-  let screenshot = 'should fail for changed screenshot.new.png';
 
   for (let browser of ['Firefox', 'Chrome', 'Chrome (mobile)'])
-    await unlink(`./tests/__screenshots__/${browser}/${screenshot}`);
+    await unlink(`./tests/__screenshots__/${browser}/${screenshot}`).catch(() => {});
 });
