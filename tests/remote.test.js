b8ce66183bbdc7682c99a66f7ba66c1030016e41
Wil Wilsman <wil@wilsman.dev>
2024-04-22 21:33:58 -0500

    ✅ Update remote test event

--- tests/remote.test.js
+++ tests/remote.test.js
@@ -4,9 +4,11 @@ import { configure } from 'moonshiner';
 configure({
   require: './tests/harness.test.js',
   plugins: [
-    test => test.emit('remote:connect', () =>
-      [1, 2].map(() => fork('./tests/harness.test.js', {
-        env: { __MOONSHINER_REMOTE__: 'process' }
-      })))
+    test => test.trigger('remote:connect', {
+      connection: () => [1, 2].map(() =>
+        fork('./tests/harness.test.js', {
+          env: { __MOONSHINER_REMOTE__: 'process' }
+        }))
+    })
   ]
 });
